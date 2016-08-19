9efe52493f15aa2ed2177a8fb0eaca41e65af465
colina118 <mikehill2010@gmail.com>
2016-08-19 00:30:12 -0500

    tarea 1 final

--- Tareas/Tarea1/maxmin.hs
+++ Tareas/Tarea1/maxmin.hs
@@ -1,26 +1,24 @@
-listup = [(,)]
-maxmin [] = 0
-maxmin (a:[]) = a
-maxmin lista = let mayor [] = 0
-		   mayor (a:[]) = a
-		   mayor (a:b:c) = let bigger (a,b) = if a>b 
-				        	      then a 
-				   		      else b 
-				   in 
-		 		    if c == [] 
-		 		    then bigger (a,b) 
-		   		    else mayor([bigger(a,b)]++c)
-		   menor [] = 0
-		   menor (a:[]) = a
-		   menor(a:b:c) = let smaller (a,b) = if a<b
-						      then a
-						      else b
-				  in 
-				   if c == []
-				   then smaller(a,b)
-				   else menor([smaller(a,b)]++c)
-		 in
-		  if lista == []
-		  then listup
-		  else listup ++ [(mayor (head lista), menor (head lista))] 
-							
+maxymin listaa = let maxmin lista = let mayor [] = 0
+                                        mayor (a:[]) = a
+                                        mayor (a:b:c) = let bigger (a,b) = if a>b
+                                                                           then a
+                                                                           else b
+                                                       in
+                                                        if c == []
+                                                        then bigger (a,b)
+                                                        else mayor([bigger(a,b)]++c)
+                                        menor [] = 0
+                                        menor (a:[]) = a
+                                        menor(a:b:c) = let smaller (a,b) = if a<b
+						      															                           then a
+						      												 			                           else b
+				  									                           in
+				   							 			                          if c == []
+				   										                          then smaller(a,b)
+				   										                          else menor([smaller(a,b)]++c)
+		 				                      in
+                                   (mayor lista, menor lista)
+                in
+                 if listaa==[]
+								 then []
+								 else [maxmin(head listaa)]++maxymin(tail listaa)
